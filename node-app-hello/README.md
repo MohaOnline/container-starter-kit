b79ebe9b3a82b996130fba40cfe4270a15fcf54a
Jun Ni <tweeg.ni@gmail.com>
2020-04-21 15:04:39 +0800

    node-app-hello-1.0.x-dev

--- node-app-hello/README.md
+++ node-app-hello/README.md
@@ -7,3 +7,11 @@ docker rm -f node-app-hello-container
 
 docker run --rm -it -p 3000:3000 --name node-app-hello-container node-app-hello
 ```
+
+安装开发依赖
+
+``` bash
+npm install nodemon --save-dev            # 代码热加载
+npm install jasmine request --save-dev    # 自动化测试
+npm install testcontainers --save-dev     # 
+```
--- node-app-hello/index.js
+++ node-app-hello/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+const domain = process.env.DOMAIN || '0.0.0.0'
+app.listen(3000, "0.0.0.0", function () {
+    console.log('Listening on port 3000!');
+    console.log('http://localhost:3000');
+});
+
 app.get('/', function (req, res) {
     res.send('Root Response through Express!');
 });
@@ -11,13 +17,34 @@ app.get('/hello', (req, res) => {
 
 const hobbies = [
     'Swimming', 'Diving', 'Jogging', 'Cooking', 'Singing'
-    ];
+];
 
 app.get('/hobbies', (req, res) => {
     res.send(hobbies);
 });
 
-app.listen(3000, "0.0.0.0", function () {
-    console.log('Listening on port 3000!');
-    console.log('http://localhost:3000');
-});
+// 数据库相关
+const host = process.env.DB_HOST || 'localhost'
+const port = process.env.DB_PORT || '5432'
+
+const {
+    Pool
+} = require('pg')
+const pool = new Pool({
+    user: 'root',
+    host: host,
+    database: 'postgres',
+    password: 'root',
+    port: parseInt(port),
+})
+
+app.get('/pg', async (req, res) => {
+    const result = await pool.query('SELECT hobby FROM hobbies')
+    res.send(result.rows);
+})
+
+app.get('/pg/:id', async (req, res) => {
+    const id = req.params.id;
+    const result = await pool.query('SELECT hobby FROM hobbies WHERE hobby_id=$1', [id])
+    res.send(result.rows[0].hobby);
+})
