3228bb9ec0f34b9d90a20862136972fc67835f26
Jun Ni <jun.ni@tanan.net.cn>
2020-06-13 16:14:13 +0800

    k8s-nginx-1.x-dev

--- k8s-nginx/README.mk
+++ k8s-nginx/README.mk
@@ -1,4 +1,5 @@
-```bash
+``` bash
+# 建删查 pod / service
 kubectl create -f nginx-pod.yml
 kubectl get pods
 kubectl delete pod/nginx-alpine
@@ -9,6 +10,20 @@ kubectl get services
 kubectl delete service/nginx-alpine
 kubectl describe service nginx-alpine
 
-kubectl run -i --tty --image=ubuntu --restart=Never --rm=true ubuntu
+# 可访问 nginx 默认页面
+lynx localhost
 
+# 新建 Pod 巡查 kubernetes service
+# 用官方 Ubuntu 镜像，进入命令行后需手动添加网络诊断工具
+kubectl run -i --tty --image=ubuntu:20.04 --restart=Never --rm=true k8s-ubuntu
+
+# 用第三方 Ubuntu 镜像自带网络诊断工具
+kubectl run -i --tty --image=ma3310/ubuntu:20.04 --restart=Never --rm=true k8s-ubuntu
+# 进入 Pod 后可通过 service 名确认服务是否工作
+curl nginx-alpine
+wget -qO - nginx-alpine
+
+# 通过 docker 工具巡查 pod
+docker container ls
+docker run --rm -it --net container:CONTAINER_ID ma3310/ubuntu:20.04
 ```
