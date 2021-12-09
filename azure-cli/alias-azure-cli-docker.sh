07edf12a7093167df5c0dc490759a86d3ea9f512
njun <tweeg.ni@gmail.com>
2021-12-10 05:51:32 +0800

    add help alias

--- azure-cli/alias-azure-cli-docker.sh
+++ azure-cli/alias-azure-cli-docker.sh
@@ -1,1 +1,3 @@
 alias daz='docker pull mcr.microsoft.com/azure-cli && docker run -it --rm -v $HOME:/root mcr.microsoft.com/azure-cli'
+alias dazpw='docker run --rm -it -v $HOME:/root mcr.microsoft.com/azure-powershell'
+
